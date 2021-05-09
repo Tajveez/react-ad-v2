abac1d0a1d8d5f7d0c70c07e508eb920778d4bbb
tajveez <tajveez786@gmail.com>
2021-05-09 23:17:18 +0500

    Setting Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import {Route} from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites'
+import NewMeetupsPage from './pages/NewMeetups' 
+
 function App() {
   return (
     <div>
+      <Route path="/">
+        <AllMeetupsPage/>
+      </Route>
+      <Route path="/new-meetups">
+        <NewMeetupsPage/>
+      </Route>
+      <Route path="/favorites">
+        <FavoritesPage/>
+      </Route>
     </div>
   );
 }
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AllMeetupsPage = () => {
     return (
         <div>
-            
+            All Meetups Page
         </div>
     )
 }
