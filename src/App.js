1197a21fb9c80868abcfada62b175165854e623a
tajveez <tajveez786@gmail.com>
2021-05-09 23:26:15 +0500

    Nav added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetups";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
-        <Route path="/new-meetups">
+        <Route path="/new-meetup">
           <NewMeetupsPage />
         </Route>
         <Route path="/favorites">
