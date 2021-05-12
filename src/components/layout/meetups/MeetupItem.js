e7a5b89007655a8739bffbb9e2b7e8e0d29cefa6
tajveez <tajveez786@gmail.com>
2021-05-12 22:57:58 +0500

    Components added

--- src/components/layout/meetups/MeetupItem.js
+++ src/components/layout/meetups/MeetupItem.js
@@ -1,16 +1,17 @@
 import itemCss from "./MeetupItem.module.css";
-const MeetupItem = ({ title, image, address, description }) => {
+const MeetupItem = ({ meetup }) => {
+  const { title, image, address, description } = meetup;
   return (
-    <li>
-      <div className="">
+    <li className={itemCss.list}>
+      <div className={itemCss.image}>
         <img src={image} alt={title} />
       </div>
-      <div>
+      <div className={itemCss.content}>
         <h3>{title}</h3>
         <address>{address}</address>
         <p>{description}</p>
       </div>
-      <div>
+      <div className={itemCss.actions}>
         <button>To Favorites</button>
       </div>
     </li>
--- src/components/layout/meetups/MeetupList.js
+++ src/components/layout/meetups/MeetupList.js
@@ -1,5 +1,14 @@
-const MeetupList = () => {
-  return <div></div>;
+import MeetupItem from "./MeetupItem";
+import listCss from "./MeetupList.module.css";
+
+const MeetupList = ({ meetups }) => {
+  return (
+    <ul className={listCss.list}>
+      {meetups.map((meetup) => {
+        return <MeetupItem key={meetup.id} meetup={meetup} />;
+      })}
+    </ul>
+  );
 };
 
 export default MeetupList;
