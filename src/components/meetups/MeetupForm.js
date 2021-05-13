30f617daa39407327bad15b4aaac8633e9f7b6bf
tajveez <tajveez786@gmail.com>
2021-05-13 22:47:46 +0500

    Form done

--- src/components/meetups/MeetupForm.js
+++ src/components/meetups/MeetupForm.js
@@ -5,7 +5,25 @@ const MeetupForm = () => {
     return (
         <Card>
             <form className={FormCss.form}>
-
+                <div className={FormCss.control}>
+                    <label htmlFor="title">Meetup Title</label>
+                    <input type="text" required id="title" />
+                </div>
+                <div className={FormCss.control}>
+                    <label htmlFor="image">Meetup Image</label>
+                    <input type="url" required id="image" />
+                </div>
+                <div className={FormCss.control}>
+                    <label htmlFor="address">Meetup Address</label>
+                    <input type="text" required id="address" />
+                </div>
+                <div className={FormCss.control}>
+                    <label htmlFor="description">Meetup Description</label>
+                    <textarea required id="description" rows="5"></textarea>
+                </div>
+                <div className={FormCss.actio}>
+                    <button>Add Meetup</button>
+                </div>
             </form>
         </Card>
     )
