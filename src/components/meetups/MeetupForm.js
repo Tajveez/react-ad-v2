010ec2fb61a9a18be248989245b22a6a3ad4fbc8
tajveez <tajveez786@gmail.com>
2021-05-13 22:41:31 +0500

    Adding form

---
+++ src/components/meetups/MeetupForm.js
@@ -0,0 +1,14 @@
+import Card from '../ui/Card/Card'
+import FormCss from './MeetupForm.module.css'
+
+const MeetupForm = () => {
+    return (
+        <Card>
+            <form className={FormCss.form}>
+
+            </form>
+        </Card>
+    )
+}
+
+export default MeetupForm
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,10 @@
+import MeetupForm from "../components/meetups/MeetupForm"
+
 const NewMeetupsPage = () => {
   return (
     <div>
       <h1>New Meetups</h1>
+      <MeetupForm/>
     </div>
   );
 };
