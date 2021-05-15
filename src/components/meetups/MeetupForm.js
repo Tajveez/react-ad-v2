0541b57cccd048cf20bed2a6fbc2ec4e418c4cfe
tajveez <tajveez786@gmail.com>
2021-05-15 23:57:50 +0500

    Form logic doen

--- src/components/meetups/MeetupForm.js
+++ src/components/meetups/MeetupForm.js
@@ -1,42 +1,60 @@
-import Card from '../ui/Card/Card'
-import FormCss from './MeetupForm.module.css'
-import {useRef} from 'react'
+import Card from "../ui/Card/Card";
+import FormCss from "./MeetupForm.module.css";
+import { useRef } from "react";
 
 const MeetupForm = () => {
-    const titleInputRef = useRef();
-    const imageInputRef = useRef();
-    const addressInputRef = useRef();
-    const descriptionInputRef = useRef();
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
-    function submitHandler(e) {
-        e.preventDefault();
-    }
+  function submitHandler(e) {
+    e.preventDefault();
+    const title = titleInputRef.current.value;
+    const image = imageInputRef.current.value;
+    const address = addressInputRef.current.value;
+    const description = descriptionInputRef.current.value;
 
-    return (
-        <Card>
-            <form className={FormCss.form} onSubmit={submitHandler}>
-                <div className={FormCss.control}>
-                    <label htmlFor="title">Meetup Title</label>
-                    <input type="text" required id="title" ref={titleInputRef} />
-                </div>
-                <div className={FormCss.control}>
-                    <label htmlFor="image">Meetup Image</label>
-                    <input type="url" required id="image" ref={imageInputRef} />
-                </div>
-                <div className={FormCss.control}>
-                    <label htmlFor="address">Meetup Address</label>
-                    <input type="text" required id="address" ref={addressInputRef} />
-                </div>
-                <div className={FormCss.control}>
-                    <label htmlFor="description">Meetup Description</label>
-                    <textarea required id="description" rows="5" ref={descriptionInputRef} ></textarea>
-                </div>
-                <div className={FormCss.actio}>
-                    <button>Add Meetup</button>
-                </div>
-            </form>
-        </Card>
-    )
-}
+    const meetupData = {
+      title,
+      image,
+      address,
+      description,
+    };
 
-export default MeetupForm
+    console.log(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={FormCss.form} onSubmit={submitHandler}>
+        <div className={FormCss.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={FormCss.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={FormCss.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={FormCss.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea
+            required
+            id="description"
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={FormCss.actio}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default MeetupForm;
