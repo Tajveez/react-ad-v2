918d7fc639e2751339c5f54475cdf74fa3762701
tajveez <tajveez786@gmail.com>
2021-05-15 23:44:04 +0500

    Refs added

--- src/components/meetups/MeetupForm.js
+++ src/components/meetups/MeetupForm.js
@@ -3,6 +3,11 @@ import FormCss from './MeetupForm.module.css'
 import {useRef} from 'react'
 
 const MeetupForm = () => {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
     function submitHandler(e) {
         e.preventDefault();
     }
@@ -12,19 +17,19 @@ const MeetupForm = () => {
             <form className={FormCss.form} onSubmit={submitHandler}>
                 <div className={FormCss.control}>
                     <label htmlFor="title">Meetup Title</label>
-                    <input type="text" required id="title" />
+                    <input type="text" required id="title" ref={titleInputRef} />
                 </div>
                 <div className={FormCss.control}>
                     <label htmlFor="image">Meetup Image</label>
-                    <input type="url" required id="image" />
+                    <input type="url" required id="image" ref={imageInputRef} />
                 </div>
                 <div className={FormCss.control}>
                     <label htmlFor="address">Meetup Address</label>
-                    <input type="text" required id="address" />
+                    <input type="text" required id="address" ref={addressInputRef} />
                 </div>
                 <div className={FormCss.control}>
                     <label htmlFor="description">Meetup Description</label>
-                    <textarea required id="description" rows="5"></textarea>
+                    <textarea required id="description" rows="5" ref={descriptionInputRef} ></textarea>
                 </div>
                 <div className={FormCss.actio}>
                     <button>Add Meetup</button>
