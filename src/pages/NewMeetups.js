030cb54c2418b5c7eacbf9868e2dc3cadd020541
tajveez <tajveez786@gmail.com>
2021-05-17 23:41:04 +0500

    Code for sending fetch to firebase done

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,8 +1,15 @@
-import MeetupForm from "../components/meetups/MeetupForm"
+import MeetupForm from "../components/meetups/MeetupForm";
 
 const NewMeetupsPage = () => {
   function addNewMeetupHandler(meetupData) {
     // Making API call to forward the form data
+    fetch("https://react-demo-ebfd9-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   return (
