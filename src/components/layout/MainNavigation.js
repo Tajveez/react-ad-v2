740a846ed55d912adb3bd2f396a2788594989c29
tajveez <tajveez786@gmail.com>
2021-05-19 23:26:26 +0500

    Count also added

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,6 +1,9 @@
 import {Link} from "react-router-dom"
 import navCss from "./MainNavigation.module.css";
+import {useContext} from 'react'
+import FavoritesContext from '../../store/favorites-context'
 const MainNavigation = () => {
+    const favoritesCtx = useContext(FavoritesContext)
     return (
         <header className={navCss.header}>
             <div className={navCss.logo}>React Meetups</div>
@@ -13,7 +16,7 @@ const MainNavigation = () => {
                         <Link to="/new-meetup">Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to="/favorites">Favorites</Link>
+                        <Link to="/favorites">Favorites ({favoritesCtx.totalFavorites})</Link>
                     </li>
                 </ul>
             </nav>
