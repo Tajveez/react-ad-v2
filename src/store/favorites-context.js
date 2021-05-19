a3008a7516fccf26c350d4f95cfbb02588b3e741
tajveez <tajveez786@gmail.com>
2021-05-19 23:06:18 +0500

    Context created

---
+++ src/store/favorites-context.js
@@ -0,0 +1,40 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+export const FavoritesContextProvider = ({ children }) => {
+  const [userFavorites, setUserFavorites] = useState(initialState);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorite) =>
+      prevUserFavorite.concat(favoriteMeetup)
+    );
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorite) =>
+      prevUserFavorite.filter((meetup) => meetup.id !== meetupId)
+    );
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavoriteHandler,
+    removeFavoriteHandler,
+    itemIsFavoriteHandler,
+  };
+  return (
+    <FavoritesContext.Provider value={context}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export default FavoritesContext;
